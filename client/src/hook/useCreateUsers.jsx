57af4f52a9810296f7581eac42a8d49ee2ec70f4
LucaseSilva <104096890+LucasESilv@users.noreply.github.com>
2024-05-10 18:54:43 -0300

    Correção no hook do criate

--- client/src/hook/useCreateUsers.jsx
+++ client/src/hook/useCreateUsers.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { createUser } from "../services/api";
+import { useUsers } from "./useUsers";
 
 export const useCreateUsers = () => {
+  const { refetchUsers } = useUsers();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -9,6 +11,7 @@ export const useCreateUsers = () => {
     setIsLoading(true);
     try {
       await createUser(userData);
+      refetchUsers();
     } catch (error) {
       setError(error);
     } finally {
